c004598a6d8464670d6dfbdf49a3e37c49bab162
Eugene Chechurin <e.chechurin@gmail.com>
2015-11-06 11:31:52 -0800

    imgaes

---
+++ app/scripts/background.js
@@ -0,0 +1,12 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId) {
+  chrome.pageAction.show(tabId);
+});
+
+console.log('Hi Y-Combinator!');
+//# sourceMappingURL=background.js.map
